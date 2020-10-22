91aa0bafa183c46749a4a90b2c5c555e434a2912
elma bt <elmabristric20@gmail.com>
2020-10-22 16:30:07 +0200

    Add project files.

---
+++ gittest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace gittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine("sqlt({0})^2 = {1}", i, Math.Sqrt (i));
+            }
+
+        }
+    }
+}
+
